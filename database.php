9176fbe8ae051633b55009a92bdb8b25939122a0
SzamanHerbaty <kuba.pietrasik1234@gmail.com>
2023-04-18 18:54:00 +0200

    projekt

---
+++ database.php
@@ -0,0 +1,20 @@
+<?php
+include_once('config.php');
+
+function getConnection() {
+    global $dbHost, $dbName, $dbUser, $dbPassword;
+    $connection = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
+    if($connection->connect_errno) {
+        $connection->close();
+        die('Database connection problem');
+    }
+    return $connection;
+}
+function getAllPosts() {
+    $connection = getConnection();
+    $sql = 'SELECT posts.id As id, posts.title As title, categories.name As categoryName From posts Inner Join categories On posts.categoryId=categories.id';
+    $result = $connection->query($sql);
+    $rows = $result->fetch_all(MYSQLI_ASSOC);
+    $connection->close();
+    return $rows;
+}
